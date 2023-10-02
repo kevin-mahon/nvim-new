3ca5948f852fdaea98e5b0ecebe69e5d080ffc87
kevin-mahon <k.mahon.dev@gmail.com>
2023-10-02 01:56:43 -0400

    init

---
+++ lua/default/init.lua
@@ -0,0 +1,4 @@
+require("default.remap")
+require("default.set")
+
+
---
+++ lua/default/remap.lua
@@ -0,0 +1,27 @@
+
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>t", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d", "<C-d>zz")
+vim.keymap.set("n", "<C-u", "<C-u>zz")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "`", "@q")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+
