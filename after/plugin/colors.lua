68f5c1200cb1b3c02a83c987db8bfa4d6f324525
Kevin <kevin.mahon@sunriseaitech.com>
2023-10-06 17:19:47 -0400

    color fix

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,14 +1,31 @@
 -- setup must be called before loading
+--
+--
+require("tokyonight").setup({
 
-function ColorMyPencil(color)
-	color = color or "tokyonight-night"
-	vim.cmd.colorscheme(color)
+  style = "night",
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+  on_highlights = function(hl, colors)
+      hl.Visual = {
+          bg = colors.red
+      }
+      hl.VisualNOS = {
+          bg = colors.red
+      }
+  end,
+  --  colors.border_highlight = colors.red 
+  --end, 
+})
+
+function FixColor(color)
+    color = color or "tokyonight-night"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 end
 
-ColorMyPencil()
+FixColor()
 
 
 
